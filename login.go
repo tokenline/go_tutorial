edb20bc7e365b34f9b500b01c24210d7e05e8944
Token Token Token <donghui.wang@ucdconnect.ie>
2023-03-01 22:14:19 +0000

    change samples into .txt preventing errors in log

--- login.go
+++ login.go
@@ -1,12 +1,56 @@
-package tutorial
+package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+type User struct {
+	index    int
+	username string
+	password string
+	gender   string
+	age      int
+	status   int
+}
 
 func main() {
 
-	fmt.Println("test")
+	var DB, err = connect()
+	if err != nil {
+		panic("failed to connect to the database")
+	}
+
+	var user User
+
+	DB.Where("id = 1").Find(&user)
+
+	fmt.Println(user)
+	user = *query(DB)
+	fmt.Println(user)
+
 }
 
-func query() {
+func connect() (gorm.DB, error) {
 
+	dsn := "root@tcp(127.0.0.1:3306)/archive?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+	if err != nil {
+		panic("failed to connect to the database")
+	}
+	fmt.Println("connect successed")
+	return *db, nil
+}
+
+func query(db gorm.DB) *User {
+	var user User
+	db.Where("username = ?", "Jake").Find(&user)
+	return &user
 }
